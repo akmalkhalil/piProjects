55a4a6a5e1dbbcdfe30450d1aeb8d866d7bcf94a
Akmal Khalil <akmal.khalil.97@gmail.com>
2021-05-30 20:54:33 +0100

    fzz diagram as I prepare to try and control 2 independant pixel strips using 1 control line and a demux

---
+++ piDemuxPixels/2strips1pi.py
@@ -0,0 +1,3 @@
+# Test using a demultiplexer with pi to control 2x ws281x LED strips
+# The main purpose of this test isn't to see if it works, I reckon it will. it's more to see how it will work and how I will need to organise my code
+# TODO: I should add to the testing functionality to test for use of Demux (up to maybe only 4 strips?)
