524a931c1501046b7349d68e33546e50b3686e87
Akmal Khalil <akmal.khalil.97@gmail.com>
2021-06-05 12:38:14 +0100

    controller buttons light up LEDs while pressed

--- piControllerInput/Untitled-1.py
+++ piControllerInput/Untitled-1.py
@@ -50,7 +50,52 @@ def rockCandyQuickTestPrint():
                     print("X")
 
 
+from gpiozero import LED
+
+def rockCandyLEDs():
+    print(gamepad)
+    ybtn = 304
+    bbtn = 305
+    abtn = 306
+    xbtn = 307
+
+    ledUp = LED(21)
+    ledLeft = LED(6)
+    ledDown = LED(16)
+    ledRight = LED(26)
+
+    for event in gamepad.read_loop():
+        if event.code == ybtn:
+            if event.value == 1:
+                ledLeft.on()
+            elif event.value == 0:
+                ledLeft.off()
+        if event.code == bbtn:
+            if event.value == 1:
+                ledDown.on()
+            elif event.value == 0:
+                ledDown.off()
+        if event.code == abtn:
+            if event.value == 1:
+                ledRight.on()
+            elif event.value == 0:
+                ledRight.off()
+        if event.code == xbtn:
+            if event.value == 1:
+                ledUp.on()
+            elif event.value == 0:
+                ledUp.off()
+
+
 if __name__ == "__main__":
     print("0 for print all button presses")
     print("1 for print EV_KEY button presses")
     print("2 for ybax test for Rock Candy Controller")
+    print("3 for rock candy controller and LEDs")
+    choice = input()
+    if choice == "0":
+        printButtons()
+    elif choice == "1": 
+        printEventKey()
+    elif choice == "2":
+        rockCandyQuickTestPrint()
